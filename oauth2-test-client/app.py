41c4a55130d087676c53a09a60080dac169c98bc
Rigel Trajano <rigel@gooee.com>
2018-06-04 10:58:58 -0400

    add form data support for testing

--- oauth2-test-client/app.py
+++ oauth2-test-client/app.py
@@ -6,6 +6,8 @@ from textwrap import dedent
 import requests
 from flask import Flask, request, abort
 
+USE_FORM_PAYLOADS = False
+
 AUTH_ENDPOINT = os.getenv('AUTH_ENDPOINT')
 TOKEN_ENDPOINT = os.getenv('TOKEN_ENDPOINT')
 VERIFY_ENDPOINT = os.getenv('VERIFY_ENDPOINT')
@@ -55,11 +57,18 @@ def get_token_response(code):
         "code": code,
         "redirect_uri": REDIRECT_URI,
     }
-    response = requests.post(
-        TOKEN_ENDPOINT,
-        auth=client_auth,
-        json=data,
-    )
+    if USE_FORM_PAYLOADS:
+        response = requests.post(
+            url=TOKEN_ENDPOINT,
+            auth=client_auth,
+            data=data,
+        )
+    else:
+        url = f'{TOKEN_ENDPOINT}?{urllib.parse.urlencode(data)}'
+        response = requests.post(
+            url,
+            auth=client_auth,
+        )
     return response.json()
 
 
@@ -71,11 +80,18 @@ def get_refreshed_token(refresh_token):
         # Optional reduction of scope
         "scope": "building:read",
     }
-    response = requests.post(
-        TOKEN_ENDPOINT,
-        auth=client_auth,
-        json=data,
-    )
+    if USE_FORM_PAYLOADS:
+        response = requests.post(
+            url=TOKEN_ENDPOINT,
+            auth=client_auth,
+            data=data,
+        )
+    else:
+        url = f'{TOKEN_ENDPOINT}?{urllib.parse.urlencode(data)}'
+        response = requests.post(
+            url,
+            auth=client_auth,
+        )
     return response.json()['access_token']
 
 
@@ -98,22 +114,29 @@ def cloud_api_callback():
         abort(403)
     auth_code = request.args.get('code')
     token_response = get_token_response(auth_code)
+    assert "access_token" in token_response
+    assert "expires_in" in token_response
+    assert "token_type" in token_response
+    assert "refresh_token" in token_response
     protected_data = get_protected_data(
         token_response['access_token'],
     )
     new_token = get_refreshed_token(token_response['refresh_token'])
     protected_data_with_new_token = get_protected_data(new_token)
+    new_token2 = get_refreshed_token(token_response['refresh_token'])
+    protected_data_with_new_token2 = get_protected_data(new_token2)
 
     return dedent(f"""\
     <p>Authorization response args: {dict(request.args)}</p>
 
-    <p>Token response: {token_response}</p>
+    <p>Token response URL PARAMS: {token_response}</p>
 
     <p>Protected Data: {protected_data}</p>
 
     <p>Refreshed Token: {new_token}</p>
 
     <p>Protected Data using new_token: {protected_data_with_new_token}</p>
+    <p>Protected Data using new_token2 from the same refresh_token: {protected_data_with_new_token2}</p>
     """)
 
 
